c9df08bf72fb20ad091f919452e4f1d0ddfda042
Paula Biazotto <pds_paula@hotmail.com>
2023-02-28 18:01:14 +0000

    added header

--- libft.h
+++ libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 17:46:41 by paula             #+#    #+#             */
+/*   Updated: 2023/02/28 17:50:33 by paula            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT
 #define LIBFT
 
@@ -8,7 +20,7 @@ int ft_isdigit(int c);
 int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
-int ft_strlen(char *str);
+int ft_strlen(const char *str);
 void* ft_memset(void *s, int c, size_t n);
 void ft_bzero(void *s, size_t n);
 
