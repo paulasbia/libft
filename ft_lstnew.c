a54bc69025c2f05154300bc4d46699e1ec060665
Paula Biazotto <pds_paula@hotmail.com>
2023-03-23 08:30:46 +0000

    update ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:58:21 by paula             #+#    #+#             */
-/*   Updated: 2023/03/22 19:06:47 by paula            ###   ########.fr       */
+/*   Updated: 2023/03/23 08:29:48 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ t_list  *ft_lstnew(void *content)
     t_list *new;
     
     new = (t_list*)malloc(sizeof(t_list));
+    if (new == 0)
+    {
+        return(NULL);
+    }
     new->content = content;
     new->next = 0;
     return(new);
